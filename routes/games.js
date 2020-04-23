56bdffd70a42ecc8287258f189973f83fac0f845
Santosh <balijasantosh@gmail.com>
2020-04-23 16:53:33 -0500

    added comments

--- routes/games.js
+++ routes/games.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router =  new express.Router();
 const {Game} = require('./../models/game');
-
+//Route for games
 router.post('/', async(req, res) => {
     const games = await Game.find({ type: req.body.game});
     res.render('games',{games: games});
